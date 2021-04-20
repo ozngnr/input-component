fbd3f6e9e591f071184c494f506ca7ab3459c952
ozngnr <ozngnr@gmail.com>
2021-04-20 15:35:49 +0100

    Update inputContainer.js

--- src/inputContainer.js
+++ src/inputContainer.js
@@ -3,10 +3,11 @@ import styled from "styled-components";
 import reactElementToJSXString from "react-element-to-jsx-string"
 
 const InputContainer = ({children, ...restProps}) => {
+  const jsxMarkup = reactElementToJSXString(children, { displayName: () => ("Input") })
 
   return (
       <Container {...restProps}>
-        <Code>{reactElementToJSXString(children)}</Code>
+        <Code>{jsxMarkup}</Code>
         {children}
       </Container>
    );
